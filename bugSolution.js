742c6d79b9a6e8aa50fe0a998f79407bc731f5ed
BugHunter-X <askzenai@gmail.com>
2025-02-23 04:16:04 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,10 @@
+```javascript
+// some javascript code that uses Tailwind CSS classes
+div.bg-red-500.custom-class {
+  /* some styles */
+}
+/*or override the specificity problem by using !important*/
+div.bg-red-500 {
+  background-color:red !important;
+}
+```
